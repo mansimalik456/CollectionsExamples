cdea5abae28e8c00ac770b21a0d96d557bfd800b
mansimalik456 <mansi.malik@oodles.io>
2022-04-18 16:25:38 +0530

    Exception Commit

---
+++ ExceptionDemoExample.java/ExceptionDemo3.java
@@ -0,0 +1,19 @@
+public class ExceptionDemo3
+{
+    
+	public static void main(String[] args) {
+        
+        try  
+        {  
+        int arr[]= {1,3,5,7};  
+        System.out.println(arr[10]); 
+        }  
+        
+        catch(ArrayIndexOutOfBoundsException e)  
+        {  
+            System.out.println("Exception : " + e);  
+        }  
+        System.out.println("rest of the code");  
+	   
+	}
+}
